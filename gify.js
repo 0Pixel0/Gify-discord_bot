d84789194a1e3560d6bff57684a12af88662b2b7
0Pixel0 <ashutosh.punia@yandex.com>
2022-04-14 15:18:55 +0530

    welcome message

--- gify.js
+++ gify.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 require("dotenv").config();
 
 const client = new Discord.Client({
-  intents: ["GUILDS", "GUILD_MESSAGES"],
+  intents: ["GUILDS", "GUILD_MESSAGES","GUILD_MEMBERS"],
 });
 
 client.on("ready", () => {
@@ -11,7 +11,14 @@ client.on("ready", () => {
 
 client.on("messageCreate", (msg) => {
   if (msg.content === "hello") {
-    msg.reply("hi");
+    
+    msg.reply(`Hi <@${msg.author.id}>`);
   }
 });
+//event listner to welcome members
+const welcomeChannel='964090441471508511';
+client.on('guildMemberAdd',(member)=>{
+    member.guild.channels.cache.get(welcomeChannel).send(`Welcome to the server <@${member.id}>`)
+})
+
 client.login(process.env.TOKEN);
